1d1585d2ce58e9d4ea20c025440a565a82635a48
Jatin-Joshi-9 <jatin090803@gmail.com>
2024-10-08 22:55:26 +0530

    3 . Repetitions

---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <string> 
+using namespace std;
+
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define PI acos(-1.0)
+
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+
+#define FOR(i, s, n) for (int i = s; i < n; i++)
+#define FORD(i, s, n) for (int i = s; i >= n; i--)
+#define EACH(x, a) for (auto& x : a)
+
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+
+#define lcm(a, b) (a / __gcd(a, b) * b)
+#define sq(x) ((x) * (x))
+#define max3(a, b, c) max(a, max(b, c))
+#define min3(a, b, c) min(a, min(b, c))
+
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
+#define readvec(v) for (auto &i : v) cin >> i;
+#define readarr(a, n) FOR(i, 0, n) cin >> a[i];
+
+#define it int
+#define ft float
+#define db double
+#define lg2 long long
+#define l2db long double
+
+int main() {
+    fast_io();
+    string str;
+    cin >> str;
+    int max = 1, count = 1;
+    FOR(i, 1, str.length()) {
+        if (str[i] == str[i-1]) {
+            count++;
+            if (count > max) max = count;
+        } else count = 1;
+    }
+    cout<<max;
+    return 0;
+}
